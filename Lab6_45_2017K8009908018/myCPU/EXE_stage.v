d7cd3fc817297b9671d16a65ca5c475dda0e8f5d
Cai Runze <cairunze17@mails.ucas.ac.cn>
2019-10-13 13:44:42 +0800

    lab6 finished

--- Lab6_45_2017K8009908018/myCPU/EXE_stage.v
+++ Lab6_45_2017K8009908018/myCPU/EXE_stage.v
@@ -49,6 +49,28 @@ wire es_res_from_hi;
 wire es_res_from_lo;
 wire [31:0]es_result;
 
+wire [31:0] es_divisor_tdata;
+wire es_divisor_tready;
+reg es_divisor_tvalid_r;
+wire es_divisor_tvalid;
+wire [31:0] es_dividend_tdata;
+wire es_dividend_tready;
+reg es_dividend_tvalid_r;
+wire es_dividend_tvalid;
+wire [63:0] es_dout_tdata;
+wire es_dout_tvalid;
+
+wire [31:0] es_divisor_tdata_u;
+wire es_divisor_tready_u;
+reg es_divisor_tvalid_u_r;
+wire es_divisor_tvalid_u;
+wire [31:0] es_dividend_tdata_u;
+wire es_dividend_tready_u;
+reg es_dividend_tvalid_u_r;
+wire es_dividend_tvalid_u;
+wire [63:0] es_dout_tdata_u;
+wire es_dout_tvalid_u;
+
 assign {es_res_from_hi ,  //143:143
         es_res_from_lo ,  //142:142
         dest_is_hi     ,  //141:141
@@ -73,6 +95,8 @@ wire [31:0] es_alu_src2   ;
 wire [31:0] es_alu_result ;
 wire [31:0] es_alu_hi_result ;
 wire [31:0] es_alu_lo_result ;
+wire [31:0] es_hi_result ;
+wire [31:0] es_lo_result ;
 wire [31:0] hi_rdata;
 wire [31:0] lo_rdata;
 wire [31:0] hi_wdata;
@@ -85,7 +109,7 @@ assign es_res_from_mem = es_load_op;
 assign es_to_ms_bus = {es_res_from_mem,  //70:70
                        es_gr_we       ,  //69:69
                        es_dest        ,  //68:64
-                       es_result  ,      //63:32  -> including: es_alu_result & (hi/lo)
+                       es_result      ,  //63:32  -> including: es_alu_result or (hi/lo)
                        es_pc             //31:0
                       };
 
@@ -111,14 +135,76 @@ assign es_alu_src1 = es_src1_is_sa  ? {27'b0, es_imm[10:6]} :
 assign es_alu_src2 = es_src2_is_imm ? {{16{es_imm[15]}}, es_imm[15:0]} : 
                      es_src2_is_8   ? 32'd8 :
                                       es_rt_value;
-//hi/lo write or read
-assign hi_wdata = (es_alu_op[12]&es_alu_op[13]&es_alu_op[14]&es_alu_op[15]&dest_is_hi)es_alu_hi_result:es_rs_value;
-assign lo_wdata = (es_alu_op[12]&es_alu_op[13]&es_alu_op[14]&es_alu_op[15]&dest_is_lo)es_alu_hi_result:es_rs_value;
+//Handle mult & div:
+wire mult_op;
+wire multu_op;
+wire div_op;
+wire divu_op;
+assign mult_op  = es_alu_op[12];
+assign multu_op = es_alu_op[13];
+assign div_op  = es_alu_op[14] ;
+assign divu_op = es_alu_op[15] ;
+
+
+//handle divid and unsigned divid
+assign es_dividend_tdata = es_alu_src1 ;
+assign es_divisor_tdata  = es_alu_src2 ;
+assign es_dividend_tdata_u = es_alu_src1 ;
+assign es_divisor_tdata_u  = es_alu_src2 ;
+assign es_divisor_tvalid = es_divisor_tvalid_r;
+assign es_dividend_tvalid = es_dividend_tvalid_r;
+assign es_divisor_tvalid_u = es_divisor_tvalid_u_r;
+assign es_dividend_tvalid_u = es_dividend_tvalid_u_r;
+
+assign es_hi_result = (div_op  && es_dout_tvalid  )?es_dout_tdata[63:32]:
+                      (divu_op && es_dout_tvalid_u)?es_dout_tdata_u[63:32]: 
+                      (mult_op || multu_op        )?es_alu_hi_result:
+                      0;
+assign es_lo_result = (div_op  && es_dout_tvalid  )?es_dout_tdata[31:0]:
+                      (divu_op && es_dout_tvalid_u)?es_dout_tdata_u[31:0]: 
+                      (mult_op || multu_op        )?es_alu_lo_result:
+                      0;
+
+
+always @(posedge clk ) begin
+    if (rst) begin
+        es_dividend_tvalid_r <=0;
+        es_divisor_tvalid_r  <=0;  
+    end
+    else if (div_op && !es_divisor_tready && !es_dividend_tready) begin
+        es_dividend_tvalid_r <=1;
+        es_divisor_tvalid_r  <=1;
+    end
+    else if (div_op && es_divisor_tready && es_dividend_tready) begin
+        es_dividend_tvalid_r <=0;
+        es_divisor_tvalid_r  <=0;
+    end
+end
+
+always @(posedge clk ) begin
+    if (reset) begin
+        es_dividend_tvalid_u_r <=0;
+        es_divisor_tvalid_u_r  <=0;  
+    end
+    else if (div_op && !es_divisor_tready && !es_dividend_tready) begin
+        es_dividend_tvalid_u_r <=1;
+        es_divisor_tvalid_u_r  <=1; 
+    end
+    else if (div_op && es_divisor_tready && es_dividend_tready) begin
+        es_dividend_tvalid_u_r <=0;
+        es_divisor_tvalid_u_r  <=0; 
+    end
+end
+
+//write or read hi/lo 
+assign hi_wdata = ((mult_op||multu_op||div_op||divu_op)&dest_is_hi)?es_hi_result:es_rs_value;
+assign lo_wdata = ((mult_op||multu_op||div_op||divu_op)&dest_is_lo)?es_lo_result:es_rs_value;
 assign es_hi_we = dest_is_hi ;
 assign es_lo_we = dest_is_lo ;
-assign es_result = res_from_hi ? hi_rdata:
-                   res_from_lo ? lo_rdata:
+assign es_result = es_res_from_hi ? hi_rdata:
+                   es_res_from_lo ? lo_rdata:
                    es_alu_result;
+
 
 alu u_alu(
     .alu_op         (es_alu_op    ),
@@ -137,6 +223,30 @@ cp0_regs u_cp0_regs(
     .lo_we      (es_lo_we),
     .wd_hi      (hi_wdata),
     .wd_lo      (lo_wdata)
+    );
+
+mydiv u_mydiv(
+    .aclk                   (clk),
+    .s_axis_divisor_tdata   (es_divisor_tdata),
+    .s_axis_divisor_tready  (es_divisor_tready),
+    .s_axis_divisor_tvalid  (es_divisor_tvalid),
+    .s_axis_dividend_tdata  (es_dividend_tdata),
+    .s_axis_dividend_tready (es_dividend_tready),
+    .s_axis_dividend_tvalid (es_dividend_tvalid),
+    .m_axis_dout_tdata      (es_dout_tdata),
+    .m_axis_dout_tvalid     (es_dout_tvalid)
+    );
+
+mydivu u_mydivu(
+    .aclk                   (clk),
+    .s_axis_divisor_tdata   (es_divisor_tdata_u),
+    .s_axis_divisor_tready  (es_divisor_tready_u),
+    .s_axis_divisor_tvalid  (es_divisor_tvalid_u),
+    .s_axis_dividend_tdata  (es_dividend_tdata_u),
+    .s_axis_dividend_tready (es_dividend_tready_u),
+    .s_axis_dividend_tvalid (es_dividend_tvalid_u),
+    .m_axis_dout_tdata      (es_dout_tdata_u),
+    .m_axis_dout_tvalid     (es_dout_tvalid_u)
     );
 
 assign data_sram_en    = 1'b1;
--- Lab6_45_2017K8009908018/myCPU/alu.v
+++ Lab6_45_2017K8009908018/myCPU/alu.v
@@ -2,7 +2,7 @@ module alu(
   input  [11:0] alu_op,
   input  [31:0] alu_src1,
   input  [31:0] alu_src2,
-  output [31:0] alu_result
+  output [31:0] alu_result,
   output [31:0] alu_hi_result,
   output [31:0] alu_lo_result
 );
